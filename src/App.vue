81c066ddbfe2422e74a204bddef0be46f97d392c
kashiki225121 <kashiki225121@gmail.com>
2021-09-26 15:48:50 +0700

    fix design

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
       dense
       dark>
 
-        <v-toolbar-title style="width:100px">Team 3</v-toolbar-title>
+        <v-toolbar-title style="width:100px">WeSite</v-toolbar-title>
         <v-tabs align-with-title>
           <v-tab
             v-for="(item, index) in menus"
@@ -21,36 +21,63 @@
       <!-- pemisah konten -->
       <v-spacer></v-spacer>
       
-      <v-list-item-avatar v-if="!guest">
-        <v-img :src="user.photo_profile ? apiDomain + user.photo_profile : 'https://randomuser.me/api/portraits/men/74.jpg'"></v-img>
-      </v-list-item-avatar>  
+      <v-menu
+        offset-y
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            icon
+            v-bind="attrs"
+            v-on="on"
+          >
+            <v-icon>mdi-account</v-icon>
+          </v-btn>
+        </template>
 
-      <v-btn color="primary" @click="login" class="mr-3" v-if="guest">
-        <v-icon left>mdi-lock</v-icon>
-        Login
-      </v-btn>
-      <v-btn color="success" @click="register" v-if="guest">
-        <v-icon left>mdi-account</v-icon>
-        Register
-      </v-btn>
+        <v-card v-if="!guest">
+          <v-list>
+            <v-list-item>
+              <v-list-item-avatar>
+                <v-img :src="user.photo_profile ? apiDomain + user.photo_profile : 'https://randomuser.me/api/portraits/men/74.jpg'"></v-img>
+              </v-list-item-avatar>
 
-      <div class="pa-2" v-if="!guest">
-        <v-btn block color="red" dark @click="logout">
-          <div v-if="onLoad == true"><v-progress-circular
-              indeterminate
-              color="black"
-              :width="1"
-              :size="15"
-          ></v-progress-circular>
-          </div>
-          <div v-else>
-              <v-icon right dark>mdi-lock-open</v-icon>
-          </div>
-          &nbsp; Logout
-        </v-btn>
-      </div>
-       
-   
+              <v-list-item-content>
+                <v-list-item-title>{{ user.name }}</v-list-item-title>
+                <v-list-item-subtitle>{{ user.email }}</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+
+          <v-divider></v-divider>
+          <v-list>
+            <v-list-item  @click="logout">
+              <div v-if="onLoad == true"><v-progress-circular
+                  indeterminate
+                  color="black"
+                  :width="1"
+                  :size="15"
+              ></v-progress-circular>
+              </div>
+              <div v-else>
+                  <v-icon left dark color="black">mdi-logout</v-icon>
+              </div>
+              &nbsp; Logout
+            </v-list-item>
+          </v-list>
+        </v-card>
+        <v-card v-else-if="guest">
+          <v-list>
+            <v-list-item @click="login">
+              <v-icon left>mdi-login</v-icon>
+              Login
+            </v-list-item>
+            <v-list-item @click="register">
+              <v-icon left>mdi-account-plus</v-icon>
+              Register
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-menu>
     </v-app-bar>
 
     <!-- Sizes your content based upon application components -->
@@ -67,7 +94,7 @@
         class="text-center"
         cols="12"
       >
-        @Team3 | VueJS 
+        @Team - 3 | WeSite 
       </v-col>
     </v-footer>
   </v-app>
